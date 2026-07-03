489166e3113387fa83fc3b32404b817a58769dfb
Zaid Liaqat <zaidliaqat999@gmail.com>
2026-07-03 05:50:34 +0500

    Configure Django Channels ASGI application with Daphne, core/routing.py, Redis channel layers connection, and environment monkeypatches

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,6 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import redis.connection
+redis.connection.DEFAULT_RESP_VERSION = 2
+
+def dummy_configure(self, *args, **kwargs):
+    self._maint_notifications_pool_handler = None
+    self._maint_notifications_connection_handler = None
+    self._oss_cluster_maint_notifications_handler = None
+    return
+
+redis.connection.AbstractConnection._configure_maintenance_notifications = dummy_configure
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -19,8 +30,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-# Placeholder for websocket routes
-websocket_urlpatterns = []
+from core.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
---
+++ backend/core/routing.py
@@ -0,0 +1,5 @@
+from django.urls import re_path
+
+websocket_urlpatterns = [
+    # WebSocket route patterns will be added here in subsequent phases
+]
